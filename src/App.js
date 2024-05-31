ccca37dcb6e66889c8e877492b2d536020789c0f
taha1214 <126785298+taha1214@users.noreply.github.com>
2024-05-31 19:11:13 +0500

    Add initial project files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React, { useState } from "react";
+
+
+function App() {
+  const [number, setNumber] = useState(0)
+
+  return (
+    <div className="App counter">
+      <h2>counter</h2>
+      <h1>{number}</h1>
+      <button className="inc" onClick={() => setNumber(number+1)}>+1</button>
+      <button className="reset"onClick={() =>setNumber(0)} >reset</button>
+      <button className="dec" onClick={() => setNumber(number-1)}>-1</button>
+
+      
+    </div>
+  );
+}
+
+export default App;
